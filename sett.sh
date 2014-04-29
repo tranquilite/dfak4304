48d323f8f9fb00b60c8ed6ccf75aa61e1c016a11
tranquilite <mathias.moe@gmail.com>
2014-04-29 15:52:54 +0200

    Oppdaterte sett.sh

--- sett.sh
+++ sett.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #Og så lager vi en nett fil som gjør det raskere å bygge ;)
+echo "Institutt for Ekstraluminal Diversjonsestetikk"
+echo -e "Verktøy for typesetting av kompendier og læringsmateriell \n"
+echo -e "\t Kjør med -s for å logge stille. \n \t Kjør med -b for å bygge bibliografibiblioteket"
+echo -e "\t GJØR DU ENDRINGER I INNHOLDSLISTA ELLER BIBLIOGRAFIEN, MÅ DU TYPESETTE TO GANGER"
+echo -e "\n \n Utformet av Seksjon for IKT-PED.  Kopirett IDE 2004-2014"
+echo -e " \n \n"
 
 cd build/
 if [ "$1" == "-s" ];
 	then
 		pdflatex ../book.tex >> pdflatex.txt
+		rm pdflatex.txt
 	else
+		echo -e "\n \n"
 		pdflatex ../book.tex
 fi
 if [ "$1" == "-b" ];
 	then
+		echo -e "\n \n"
 		bibtex ../book.tex >> bibtex.txt
 		pdflatex ../book.tex >> pdflatex.txt
 fi
